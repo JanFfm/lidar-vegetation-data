b4607bf4b53e7bb3e76999bb28c34a6d6643272b
JanFfm <45403124+JanFfm@users.noreply.github.com>
2022-05-09 12:24:57 +0200

    Update plot_las.py

--- plot_las.py
+++ plot_las.py
@@ -17,7 +17,7 @@ for file in Path(path).glob('*.laz'):
     #print(file.name)
     las_file_path.append(file)
     
-las = laspy.read(las_file_path[0])
+las = laspy.read(las_file_path[1])
 color_map = np.array(las['classification'])
 color_map = color_map / color_map.max() # scale to [0 ,.., 1]
 color_map = np.array([color_map, color_map, color_map]).transpose((1, 0))
@@ -35,65 +35,3 @@ geom.points = o3d.utility.Vector3dVector(point_data)
 geom.colors = o3d.utility.Vector3dVector(color_map)
 o3d.visualization.draw_geometries([geom])
 
-
-"""
-
-path="./gelsenkirchen"
-
-#read all .laz from current working dir
-las_file_path = []
-for file in Path(path).glob('*.laz'):
-    #print(file.name)
-    las_file_path.append(file)
-    
-las = laspy.read(las_file_path[0])
-print(las.header)  
-#<LasHeader(1.2, <PointFormat(1, 0 bytes of extra dims)>)>
-labels = list(las.point_format.dimension_names)
-point_number = len(las.points)
-print("number of points: ", point_number)
-print(labels)
-
-print(las.xyz[0])
-print(las.header.global_encoding)
-print(las.header.x_max)
-print(las.header.x_min)
-print(las.header.y_max)
-print(las.header.y_min)
-print(las.header.file_source_id)
-print(las.header.DEFAULT_POINT_FORMAT)
-print(las.X)
-print(las.Y)
-print(las.header.extra_header_bytes)
-"""
-"""
-
-points = las.points.copy()
-
-#scale [0.01 0.01 0.01]
-#offet[     -0. 5000000.      -0.]
-
-geom = o3d.geometry.PointCloud()
-geom.points = o3d.utility.Vector3dVector(points)
-o3d.visualization.draw_geometries([geom])
-"""
-"""
-# getting scaling and offset parameters
-las_scaleX = las.header.scale[0]
-las_offsetX = las.header.offset[0]
-las_scaleY = las.header.scale[1]
-las_offsetY = las.header.offset[1]
-las_scaleZ = las.header.scale[2]
-las_offsetZ = las.header.offset[2]
-
-# calculating coordinates
-p_X = np.array((points['X'] * las_scaleX) + las_offsetX)
-p_Y = np.array((points['Y'] * las_scaleY) + las_offsetY)
-p_Z = np.array((points['Z'] * las_scaleZ) + las_offsetZ)
-
-# plotting points
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(p_X, p_Y, p_Z, c='r', marker='o')
-plt.show()
-"""
