72d208c5b142b527fc15898de38dc2bcc0778fe7
JanFfm <45403124+JanFfm@users.noreply.github.com>
2022-07-05 17:54:13 +0200

    bla

--- coord_f.py
+++ coord_f.py
@@ -12,9 +12,7 @@ def utm_to_lat_long(x, y, section=32):
     Returns:
         _type_: _description_
     """
-    print(x, " ", y)
     latlong = utm.to_latlon(x,y,section, northern=True)
-    print(latlong)
     return latlong
 
 
