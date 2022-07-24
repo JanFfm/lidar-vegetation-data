0c5aa5432f50c7eb8924823db05ead475e80719d
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2022-07-24 17:50:10 +0200

    return danger score

--- weather_api.py
+++ weather_api.py
@@ -5,18 +5,18 @@ weather data from API: api.openweathermap.org
 1. Read a las file (function defined in: get_points())
 2. Extract coordinates of las file and convert them from utm into lat,lon (defined in: get_coordinates())
 3. Call Weather API at coordinates and check for heat, storm, drought (defined in: get_weather(), check_storm(), check_heat())
-4. If there is an alert, check if there is a tree in the area, and set rgb values for the points in tree
-    write the points into another las file and return it. 
+4. return danger score for coordinate (defined in get_danger())
 
 """
 from urllib import response
+from matplotlib.pyplot import get
 import requests
 import json
 import coord_f
 import laspy
 import numpy
-import colorful_alerts
 import check_tree
+
 # read a las file and return all points in numpy array
 def get_points(las_file):
     las = laspy.read(las_file)
@@ -72,15 +72,9 @@ def call_weather_alerts(api_response):
     warning = api_response.get('warnings_nowcast')
     return warning
 
-# take lidar points as numpy array and extract location, call API for that location and check for weather alerts
-def get_weather(las):
-    # get up-left coordinates in las-file:
+# return danger score for a coordinate Score range: 0 to 6
+def get_danger(lon, lat, class_of_tree):
     danger = 0
-    las_points = get_points(las)
-    coordinates = get_coordinates(las_points)
-    lat = coordinates[0]
-    lon = coordinates[1]
-    # Call API for requested location
     base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang=de&appid=d1eb6e68d2df1de466fdf3a936b966dd"
     api_response = call_api(base_url)
     # check if it is stormy and increment danger score
@@ -91,63 +85,22 @@ def get_weather(las):
     heat = check_heat((api_response))
     if heat: 
         danger += 1
-    # if there is storm or heat, check if there is high vegetation in lidar file:
-    if storm or heat: 
-        # las_points is a numpy array, and we want to check for each entry, if the point is classified as high vegetation (classification number 5)
-        for point in las_points:
-            if point.classification == 5 and danger == 1:
-                # request species or family of given tree and calculate risk --> wo ist die denn gespeichert?
-                if point.tree_class in check_tree.very_well_adapted_dryness:
-                    # yellow
-                    point.red = 255
-                    point.green = 255
-                    point.blue = 102
-                elif point.tree_class in check_tree.well_adapted_dryness:
-                    # orange
-                    point.red = 255
-                    point.green = 204
-                    point.blue = 153
-                elif point.tree_class in check_tree.moderately_well_adapted_dryness:
-                    # dark orange
-                    point.red = 255
-                    point.green = 128
-                    point.blue = 0
-                elif point.tree_class in check_tree.not_adapted_dryness:
-                    # red
-                    point.red = 255
-                    point.green = 0
-                    point.blue = 0
-            elif point.classification == 5 and danger == 1:
-                # request species or family of given tree and calculate risk --> wo ist die denn gespeichert?
-                if point.tree_class in check_tree.very_well_adapted_dryness:
-                    # orange
-                    point.red = 255
-                    point.green = 204
-                    point.blue = 153
-                elif point.tree_class in check_tree.well_adapted_dryness:
-                    # dark orange
-                    point.red = 255
-                    point.green = 128
-                    point.blue = 0
-
-                elif point.tree_class in check_tree.moderately_well_adapted_dryness:
-                    # red
-                    point.red = 255
-                    point.green = 0
-                    point.blue = 0    
-                elif point.tree_class in check_tree.not_adapted_dryness:
-                    # violet
-                    point.red = 153
-                    point.green = 0
-                    point.blue = 153
-    return las_points[0].red   
-    #return storm, heat
-
+    if class_of_tree in check_tree.very_well_adapted_dryness:
+        danger += 1
+    if class_of_tree in check_tree.well_adapted_dryness:
+        danger += 2
+    if class_of_tree in check_tree.moderately_well_adapted_dryness:
+        danger += 3
+    if class_of_tree in check_tree.not_adapted_dryness:
+        danger += 4
+    # return danger score 
+    return danger
 
 def main():
     lidar_file = "/home/masch/Dokumente/Uni/datachallenges/git/lidar-vegetation-data/lidar-files/wesel_buildings/3dm_32_326_5728_1_nw.las"
-    print(get_weather(lidar_file))
-    #colorful_alerts.colorize_alert(lidar_file, 5, 100,100,100)
+    las_points = get_points(lidar_file)
+    coordinates = get_coordinates(las_points)
+    print(get_danger(coordinates[0], coordinates[1], "Fichte"))
 
 if __name__ == '__main__':
     main()  
