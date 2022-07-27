f45d1a5415262c0bd60b8ef7bf5afad20c2b9816
JanFfm <45403124+JanFfm@users.noreply.github.com>
2022-07-27 20:14:23 +0200

    commenting

--- make_train_data.py
+++ make_train_data.py
@@ -6,50 +6,65 @@ from PIL import Image
 import os
 
 def make_featue_set(df_list, savepath='feature_list_color.csv', extract_colors=False):
-    individuals = []
-    features_list = []
-    print("extracting trees:")    
-    for df in tqdm(df_list):     
-            cluster_ids = np.unique(np.array(df['Cluster_ID']).astype(int))
-            for id in cluster_ids:
-                    df_split =df[df['Cluster_ID'] == id]
-                    individuals.append(df_split)
-    
-    print("extracting features:")
-    for individuum in tqdm(individuals):
-        features = [np.unique(np.array(df['GATTUNGS_ID']).astype(int))[0]]
-        for df in [individuum,individuum[individuum.x > np.percentile(individuum.x ,75)] ,individuum[individuum.x < np.percentile(individuum.x ,25)], individuum[(np.percentile(individuum.x ,25) < individuum.x) & (individuum.x < np.percentile(individuum.x ,75))], individuum[individuum.y > np.percentile(individuum.y ,75)],individuum[individuum.y < np.percentile(individuum.y ,25)], individuum[(np.percentile(individuum.y ,25) < individuum.y) & (individuum.y < np.percentile(individuum.y ,75))], individuum[individuum.z > np.percentile(individuum.z ,75)],individuum[individuum.z < np.percentile(individuum.z ,25)], individuum[(np.percentile(individuum.z ,25) < individuum.z) & (individuum.z < np.percentile(individuum.z ,75))]]:
-                
-                xs = df['x']
-                ys = df['y']
-                zs = df['z']
-                features +=[len(xs)]
-                if extract_colors:
-                        r  = df['red']
-                        g  = df['green']
-                        b  = df['blue']
-                        nir  = df['nir']
-                        all = list(zip(xs,ys,zs,r,g,b,nir))
-                        features += extract([xs,ys,zs,r,g,b,nir])
-                else:
-                        all = list(zip(xs,ys,zs))
-                        features += extract([xs,ys,zs])
-                all=np.array(all)
+        """_summary_
 
-        keys = ['feature_' + str(i) for i in range(len(features))]
-        keys[0] = 'ID_GATTUNG'
-        dic = dict(zip(keys, features))
+        Args:
+        df_list (list): list of pandas.DataFrame
+        savepath (str, optional): _description_. Defaults to 'feature_list_color.csv'.
+        extract_colors (bool, optional): _description_. Defaults to False.
+        """
+        individuals = []
+        features_list = []
+        print("extracting trees:")    
+        for df in tqdm(df_list):     
+                cluster_ids = np.unique(np.array(df['Cluster_ID']).astype(int))
+                for id in cluster_ids:
+                        df_split =df[df['Cluster_ID'] == id]
+                        individuals.append(df_split)
         
+        print("extracting features:")
+        for individuum in tqdm(individuals):
+                features = [np.unique(np.array(df['GATTUNGS_ID']).astype(int))[0]]
+                for df in [individuum,individuum[individuum.x > np.percentile(individuum.x ,75)] ,individuum[individuum.x < np.percentile(individuum.x ,25)], individuum[(np.percentile(individuum.x ,25) < individuum.x) & (individuum.x < np.percentile(individuum.x ,75))], individuum[individuum.y > np.percentile(individuum.y ,75)],individuum[individuum.y < np.percentile(individuum.y ,25)], individuum[(np.percentile(individuum.y ,25) < individuum.y) & (individuum.y < np.percentile(individuum.y ,75))], individuum[individuum.z > np.percentile(individuum.z ,75)],individuum[individuum.z < np.percentile(individuum.z ,25)], individuum[(np.percentile(individuum.z ,25) < individuum.z) & (individuum.z < np.percentile(individuum.z ,75))]]:
+                        
+                        xs = df['x']
+                        ys = df['y']
+                        zs = df['z']
+                        features +=[len(xs)]
+                        if extract_colors:
+                                r  = df['red']
+                                g  = df['green']
+                                b  = df['blue']
+                                nir  = df['nir']
+                                all = list(zip(xs,ys,zs,r,g,b,nir))
+                                features += extract([xs,ys,zs,r,g,b,nir])
+                        else:
+                                all = list(zip(xs,ys,zs))
+                                features += extract([xs,ys,zs])
+                        all=np.array(all)
 
-        features_list.append(dic)
-       
-    with open(savepath, 'w', newline='') as csvfile:        
-        writer = csv.DictWriter(csvfile, keys, extrasaction='ignore')
-        writer.writeheader()                
-        writer.writerows(features_list)
+                keys = ['feature_' + str(i) for i in range(len(features))]
+                keys[0] = 'ID_GATTUNG'
+                dic = dict(zip(keys, features))
+                
+
+                features_list.append(dic)
+        
+        with open(savepath, 'w', newline='') as csvfile:        
+                writer = csv.DictWriter(csvfile, keys, extrasaction='ignore')
+                writer.writeheader()                
+                writer.writerows(features_list)
 
 
 def extract(values):
+        """_summary_
+
+        Args:
+        values (_type_): _description_
+
+        Returns:
+        _type_: _description_
+        """
         features = []
         for ax in values:
                 features += [np.mean(ax), np.median(ax), np.min(ax), np.max(ax),np.std(ax)]
@@ -59,6 +74,15 @@ def extract(values):
 
 
 def make_images(list_of_points, gattung, save_path, counter):
+        """_summary_
+
+        Args:
+            list_of_points (_type_): _description_
+            gattung (_type_): _description_
+            save_path (_type_): _description_
+            counter (_type_): _description_
+        """
+        
         list_of_points = np.array(list_of_points)
         
         xz = np.zeros([256,256])        
--- visualize.py
+++ visualize.py
@@ -4,38 +4,19 @@ import random
 from tqdm import tqdm
 from colors import get_classification_color
 
-
-"""
-    las_file_path = []
-    path = "./gelsenkirchen"
-    for file in Path(path).glob('*.laz'):
-        #print(file.name)
-        las_file_path.append(file)
-        
-Based on https://medium.com/spatial-data-science/an-easy-way-to-work-and-visualize-lidar-data-in-python-eed0e028996c
-
-color elem of: ['X', 'Y', 'Z', 'intensity', 'return_number', 'number_of_returns', 'scan_direction_flag',  'edge_of_flight_line', 'classification', 'synthetic', 'key_point', 'withheld', 'scan_angle_rank', 'user_data', 'point_source_id', 'gps_time'] 
-"""
 def plot_las_3d(las, color=None):
     """
-    """        
-    #las = laspy.read(file)
-    #color_map = np.array(las[color])
-    #color_map = color_map / color_map.max() # scale to [0 ,.., 1]
-    #color_map = np.array([color_map, color_map, color_map]).transpose((1, 0))
+    build 3D Visualisation of lidar Data. Creates color channel from rgb or classification values
+    """
+
     if color == None or color == 'rgb':
         color_map = np.array([las.points['red'] /255, las.points['green'] /255, las.points['blue'] /255]).transpose((1, 0))
     elif color=='nir':
         color_map = np.array([las.points['nir'] /255, las.points['red'] /255, las.points['blue'] /255]).transpose((1, 0))
-
         
     elif color =="classification":
         color_map = np.array(list(map(get_classification_color, las.points[color])))
-        #alt_map = np.ones([1, len(color)]) /2
-        #print("colr_ap", color_map)
-        #print("alt_ap", alt_map)
-       # color_map = color_map / color_map.max() # scale to [0 ,.., 1]
-        #color_map = np.array([color_map, color_map, color_map]).transpose((1, 0))
+
     
         
     print(color_map)
@@ -49,35 +30,43 @@ def plot_las_3d(las, color=None):
     geom.colors = o3d.utility.Vector3dVector(color_map)
     o3d.visualization.draw_geometries([geom])
  
-"""
-can set 'category' or other las-index as Z dimension, which is also default color map
-much faster than plot 3d 
-color elem of: ['X', 'Y', 'Z', 'intensity', 'return_number', 'number_of_returns', 'scan_direction_flag',  'edge_of_flight_line', 'classification', 'synthetic', 'key_point', 'withheld', 'scan_angle_rank', 'user_data', 'point_source_id', 'gps_time'] 
-"""    
+
 def plot_las_2d(las, color='classification'):
-    """_summary_
+    """2d visualisation of lidar data (faster then 3d)
 
     Args:
-        las (_type_): _description_
-        color (str, optional): _description_. Defaults to 'classification'.
+        las (laspy.las): las_ffiel
+        color (str, optional): lidar-value groupt to colorize data. Defaults to 'classification'.
     """
     point_data = np.stack([las.X, las.Y,las[color]], axis=0).transpose((1, 0))
     geom = o3d.geometry.PointCloud()
     geom.points = o3d.utility.Vector3dVector(point_data)
     o3d.visualization.draw_geometries([geom])
-    
-#las= laspy.read("gelsenkrichen classified/test1color.laz")
-#plot3d(las)
+
 
 def label_to_color_map(labels):
-     labels = np.array(labels)
-     label_number = labels.max() +1 
-     label_colors =  np.array([[random.randint(0,255), random.randint(0,255), random.randint(0,255)] for i in tqdm(range(label_number))]) / 255
-     print("label_colors", label_colors.shape)
-     return  np.array([[label_colors[labels[i]][0], label_colors[labels[i]][1],label_colors[labels[i]][2]] for i in tqdm(range(len(labels)))])
+    """creates a color map for a list of labels
+
+    Args:
+        labels (lsit): list of labels
+
+    Returns:
+        list: coor map with color for each label
+    """
+    labels = np.array(labels)
+    label_number = labels.max() +1 
+    label_colors =  np.array([[random.randint(0,255), random.randint(0,255), random.randint(0,255)] for i in tqdm(range(label_number))]) / 255
+    print("label_colors", label_colors.shape)
+    return  np.array([[label_colors[labels[i]][0], label_colors[labels[i]][1],label_colors[labels[i]][2]] for i in tqdm(range(len(labels)))])
 
 
 def draw_point_cloud(points, colors):
+    """makes the visualisation i open§D
+
+    Args:
+        points (list): point cloud
+        colors (list): color values for each point
+    """
     geom = o3d.geometry.PointCloud()
     geom.points = o3d.utility.Vector3dVector(points)
     geom.colors = o3d.utility.Vector3dVector(colors)
@@ -85,6 +74,18 @@ def draw_point_cloud(points, colors):
 
 
 def merge_class_with_labels(points, labels, inices, classifications):
+    """paints each point according to their classification
+    afterwards, paint each cluster in a certain color
+
+    Args:
+        points (list): list of 3d points
+        labels (list): list of labeling for each point
+        inices (list): inidices of points i riginal lidar data
+        classifications (list): classification values
+
+    Returns:
+        list: color map for each point
+    """
     points, labels, indices= zip(*((p, l, i) for p, l,i in zip(points, labels, inices) if l > -1))
     points = np.array(points)
     labels = np.array(labels)
@@ -110,6 +111,18 @@ def merge_class_with_labels(points, labels, inices, classifications):
 
 
 def merge_colors_with_labels(points, labels, inices, rgb):
+    """paints each point according to their r g b values
+    afterwards, paint each cluster in a certain color
+
+    Args:
+        points (list): list of 3d points
+        labels (list): list of labeling for each point
+        inices (list): inidices of points i riginal lidar data
+        rgb (list): rgb values for each point
+
+    Returns:
+        list: color map for each point
+    """
     points, labels, indices= zip(*((p, l, i) for p, l,i in zip(points, labels, inices) if l > -1))
     points = np.array(points)
     labels = np.array(labels)
